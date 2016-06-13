fc3968ed5f9e226c59cc1d8731cf9fd14841b436
LCarvalhoUIS <lucasccb@ig.com.br>
2016-06-13 15:30:06 -0500

    Added Roamer Checker

---
+++ Roamer Checker.lua
@@ -0,0 +1,31 @@
+--Roamer Checker by Lucas C.
+foundLegends=false
+
+while true do
+  --Note: this only works in USA Gold/Silver
+  --Reverse here because of the different endianness between player and legend location
+  MyLocation=string.reverse(string.format("%02X",memory.readwordunsigned(0x0000DD2F)))
+  
+  
+  legendA=string.format("%02X", memory.readwordunsigned(0x0000DD1C))
+  legendB=string.format("%02X", memory.readwordunsigned(0x0000DD23))
+  legendC=string.format("%02X", memory.readwordunsigned(0x0000DD2A))
+ 
+  
+  
+   gui.text(0,10,"Player: "..MyLocation)
+   gui.text(0,20,"Legend A: "..legendA)
+   gui.text(0,30,"Legend B: "..legendB)
+   gui.text(0,40,"Legend C: "..legendC)
+
+
+   
+   if MyLocation == legendA or MyLocation == legendB or MyLocation == legendC or MyLocation == Thefuckisthis then
+     gui.text(0,00,"Legend is on this route!", "red")
+     foundLegends=true
+     print("found it")
+     emu.pause()
+   end
+   
+
+  end
