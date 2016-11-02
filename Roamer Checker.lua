1a1af5e1ea7fbac713b8f28d2f5a14011e5a1179
Lucas C <lucasccb@ig.com.br>
2016-11-02 15:16:23 -0500

    Code cleanup

--- Roamer Checker.lua
+++ Roamer Checker.lua
@@ -1,31 +1,19 @@
 --Roamer Checker by Lucas C.
-foundLegends=false
 
 while true do
-  --Note: this only works in USA Gold/Silver
+  --Note: this only works in USA Gold/Silver. Addresses need to bechanged accordingly.
   --Reverse here because of the different endianness between player and legend location
   MyLocation=string.reverse(string.format("%02X",memory.readwordunsigned(0x0000DD2F)))
-  
-  
   legendA=string.format("%02X", memory.readwordunsigned(0x0000DD1C))
   legendB=string.format("%02X", memory.readwordunsigned(0x0000DD23))
   legendC=string.format("%02X", memory.readwordunsigned(0x0000DD2A))
- 
-  
-  
-   gui.text(0,10,"Player: "..MyLocation)
-   gui.text(0,20,"Legend A: "..legendA)
-   gui.text(0,30,"Legend B: "..legendB)
-   gui.text(0,40,"Legend C: "..legendC)
 
-
+  gui.text(0,10,"Player: "..MyLocation)
+  gui.text(0,20,"Legend A: "..legendA)
+  gui.text(0,30,"Legend B: "..legendB)
+  gui.text(0,40,"Legend C: "..legendC)
    
-   if MyLocation == legendA or MyLocation == legendB or MyLocation == legendC or MyLocation == Thefuckisthis then
-     gui.text(0,00,"Legend is on this route!", "red")
-     foundLegends=true
-     print("found it")
-     emu.pause()
-   end
-   
-
+  if MyLocation == legendA or MyLocation == legendB or MyLocation == legendC then
+    gui.text(0,00,"Legend is on this route!", "red")
   end
+end
